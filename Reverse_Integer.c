76acb632d6bc28331774b51abedbbaeaeb769d42
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-02-04 10:25:45 +0530

    #include<stdio.h> int main() {     int n,d,rev=0,c=0,temp;     scanf("%d",&n);     temp=n;     while(n)     {         d=n%10;         if(d==0)         c++;         rev=(rev*10)+d;         n=n/10;     }     printf("%d",rev);     return 0; }

---
+++ Reverse_Integer.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,d,rev=0,c=0,temp;
+    scanf("%d",&n);
+    temp=n;
+    while(n)
+    {
+        d=n%10;
+        if(d==0)
+        c++;
+        rev=(rev*10)+d;
+        n=n/10;
+    }
+    printf("%d",rev);
+    return 0;
+}
