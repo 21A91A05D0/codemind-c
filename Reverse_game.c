717404296fd320ff1f6fbc2c092becf82440af14
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-04-21 01:30:27 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,c=0,d,sum=0,rev=0,temp;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         rev=0;         temp=arr[i];         while(arr[i]>0)         {             d=arr[i]%10;             rev=rev*10+d;             arr[i]=arr[i]/10;         }         arr[i]=rev;     }     for(i=0;i<n;i++)         {             printf("%d ",arr[i]);         }
    }

---
+++ Reverse_game.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,c=0,d,sum=0,rev=0,temp;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        rev=0;
+        temp=arr[i];
+        while(arr[i]>0)
+        {
+            d=arr[i]%10;
+            rev=rev*10+d;
+            arr[i]=arr[i]/10;
+        }
+        arr[i]=rev;
+    }
+    for(i=0;i<n;i++)
+        {
+            printf("%d ",arr[i]);
+        }
+    
+}
