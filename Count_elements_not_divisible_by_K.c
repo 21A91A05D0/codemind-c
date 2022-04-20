fa5fa7647641779c8db1321af61912a45f567bdd
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-04-21 01:45:16 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,k,c=0;     scanf("%d",&n);     scanf("%d",&k);     for(i=1;i<=n;i++)     {         scanf("%d",&arr[i]);     }     for(i=1;i<=n;i++)     {         if(arr[i]%k!=0)         {             c++;         }     }     printf("%d",c);     return 0; }

---
+++ Count_elements_not_divisible_by_K.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,k,c=0;
+    scanf("%d",&n);
+    scanf("%d",&k);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=1;i<=n;i++)
+    {
+        if(arr[i]%k!=0)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    return 0;
+}
