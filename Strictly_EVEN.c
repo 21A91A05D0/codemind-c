37a940d538ca6c3183819be5a246bfa0046fe165
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-04-26 19:33:53 +0530

    #include<stdio.h> int main() { 	int n,i,arr[100],c=0; 	scanf("%d",&n); 	for(i=0;i<n;i++) 	{ 		scanf("%d",&arr[i]); 	} 	for(i=1;i<n;i+=2) 	{ 	    {
    		if(arr[i]%2==0)
    		{
    		    c++;
    			printf("False");
         	}
    	}
    	
    	}
    	if(c==0)
    	{
    		printf("True");
    	}
    	return 0;
    	
    }

---
+++ Strictly_EVEN.c
@@ -0,0 +1,29 @@
+
+#include<stdio.h>
+int main()
+{
+	int n,i,arr[100],c=0;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=1;i<n;i+=2)
+	{
+	    {
+	   
+		if(arr[i]%2==0)
+		{
+		    c++;
+			printf("False");
+     	}
+	}
+	
+	}
+	if(c==0)
+	{
+		printf("True");
+	}
+	return 0;
+	
+}
