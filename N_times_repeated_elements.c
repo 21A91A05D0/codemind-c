061e8f2515bbda226c6ae40446947d831eb81158
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-05-02 00:42:13 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,t=0,c=0,d=0,k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         c=1;         for(int j=i+1;j<n;j++)         {         	if(arr[i]==0)         	{         		break; 			}             if(arr[i]==arr[j])             {                 arr[j]=0;                 c++;             }         }         if(k==c)         {             d++;             printf("%d ",arr[i]);         }     }     if(d==0)     {         printf("-1");     } }

---
+++ N_times_repeated_elements.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,t=0,c=0,d=0,k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        c=1;
+        for(int j=i+1;j<n;j++)
+        {
+        	if(arr[i]==0)
+        	{
+        		break;
+			}
+            if(arr[i]==arr[j])
+            {
+                arr[j]=0;
+                c++;
+            }
+        }
+        if(k==c)
+        {
+            d++;
+            printf("%d ",arr[i]);
+        }
+    }
+    if(d==0)
+    {
+        printf("-1");
+    }
+}
