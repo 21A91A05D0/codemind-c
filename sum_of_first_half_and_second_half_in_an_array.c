fe4cb160a1823c583340598cc842e1038f936c40
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-05-02 02:13:26 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,j,k,c=0,pum=0,sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n/2;i++)     {         sum=sum+arr[i];     }     for(j=(n/2);j<n;j++)     {         pum=pum+arr[j];     }     printf("%d %d",sum,pum); }

---
+++ sum_of_first_half_and_second_half_in_an_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,j,k,c=0,pum=0,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n/2;i++)
+    {
+        sum=sum+arr[i];
+    }
+    for(j=(n/2);j<n;j++)
+    {
+        pum=pum+arr[j];
+    }
+    printf("%d
+%d",sum,pum);
+}
