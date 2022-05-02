026e1c4a2f77926d7ca5c01ed6ae7c247881c75e
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-05-02 21:34:32 +0530

    #include<stdio.h> int main() {     int i,n,k=0,d,c=0,arr[100],f=0,t=0,a[100];     int sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=1;         for(int j=0;j<n;j++)         {             if(i!=j)             {                 if(arr[i]==arr[j])                 {                     c++;                     arr[j]=-1;                 }             }         }         if(c>=1)         {             a[k]=arr[i];             k++;         }     }     for(i=0;i<k;i++)     {         if(a[i]%2==0)         {             t++;         }     }     printf("%d",t);
    }

---
+++ unique_even_count.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,k=0,d,c=0,arr[100],f=0,t=0,a[100];
+    int sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=1;
+        for(int j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+                if(arr[i]==arr[j])
+                {
+                    c++;
+                    arr[j]=-1;
+                }
+            }
+        }
+        if(c>=1)
+        {
+            a[k]=arr[i];
+            k++;
+        }
+    }
+    for(i=0;i<k;i++)
+    {
+        if(a[i]%2==0)
+        {
+            t++;
+        }
+    }
+    printf("%d",t);
+    
+}
