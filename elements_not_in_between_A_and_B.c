d93838bc73b3b9b5854a222b510ebee53e39a3cd
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-05-03 18:59:52 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],c=0,a,b,d=0,sum=0,ar[100],k=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         //printf("%d ",arr[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]<a)         {             ar[k]=arr[i];             k++;         }         if(arr[i]>b)         {             ar[k++]=arr[i];         }     }     for(i=0;i<k;i++)     {         printf("%d ",ar[i]);     }     if(k==0)     {         printf("-1");     } }

---
+++ elements_not_in_between_A_and_B.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],c=0,a,b,d=0,sum=0,ar[100],k=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        //printf("%d ",arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]<a)
+        {
+            ar[k]=arr[i];
+            k++;
+        }
+        if(arr[i]>b)
+        {
+            ar[k++]=arr[i];
+        }
+    }
+    for(i=0;i<k;i++)
+    {
+        printf("%d ",ar[i]);
+    }
+    if(k==0)
+    {
+        printf("-1");
+    }
+}
