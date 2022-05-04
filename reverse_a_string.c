a3e81c06aa972eae2ed94f38a0db6b3a79fbe0ee
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-05-04 20:34:44 +0530

    #include<stdio.h> #include<string.h> int main() {     char str[100],i,l;     scanf("%[^ ]",str);     l=strlen(str);     for(i=l-1;i>=0;i--)     {         printf("%c",str[i]);     }    // printf("%s",str);
    }

---
+++ reverse_a_string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100],i,l;
+    scanf("%[^
+]",str);
+    l=strlen(str);
+    for(i=l-1;i>=0;i--)
+    {
+        printf("%c",str[i]);
+    }
+   // printf("%s",str);
+    
+}
