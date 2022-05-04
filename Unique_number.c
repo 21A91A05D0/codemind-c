10ba4925b52e32a5e12382491f8695fca8c67bc8
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-05-04 21:22:23 +0530

    #include<stdio.h> int main() {     int a[100],k=0,n,i,d,c=0,f=0;     scanf("%d",&n);     while(n)     {         d=n%10;         a[k++]=d;         n=n/10;     }     for(i=0;i<k;i++)     {         c=0;         for(int j=0;j<k;j++)         {             if(a[i]==a[j])             {                 c++;             }         }         if(c>1)         {             f=1;             printf("Not Unique Number");             break;         }     }     if(f==0)     {         printf("Unique Number");     } }

--- Unique_number.c
+++ Unique_number.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,k=0,d,c=0,arr[100],f=0;
+    int a[100],k=0,n,i,d,c=0,f=0;
     scanf("%d",&n);
     while(n)
     {
         d=n%10;
-        arr[k++]=d;
+        a[k++]=d;
         n=n/10;
     }
     for(i=0;i<k;i++)
@@ -14,7 +14,7 @@ int main()
         c=0;
         for(int j=0;j<k;j++)
         {
-            if(arr[i]==arr[j])
+            if(a[i]==a[j])
             {
                 c++;
             }
