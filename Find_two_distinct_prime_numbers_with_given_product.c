1faae55ebce90a52d9bff1b7bed0262d8ede0bd5
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-05-18 11:52:47 +0530

    #include<stdio.h> int isprime(int n) {     int i,c=0;     for(i=1;i<=n;i++)     {         if(n%i==0)         {             c++;         }     }     if(c==2)     {         return n;     }     return 0;
    }
    int main()
    {
        int n,i,j,c=0;
        scanf("%d",&n);
        for(i=1;i<=n;i++)
        {
            for(j=1;j<=n;j++)
            {
                if(isprime(i)*(isprime(j))==n)
                {
                    c=1;
                    printf("%d ",i);
                    break;
                }
            }
        }
        if(c==0)
        {
            printf("-1");
        }
        
    }

---
+++ Find_two_distinct_prime_numbers_with_given_product.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int isprime(int n)
+{
+    int i,c=0;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            c++;
+        }
+    }
+    if(c==2)
+    {
+        return n;
+    }
+    return 0;
+    
+}
+int main()
+{
+    int n,i,j,c=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(isprime(i)*(isprime(j))==n)
+            {
+                c=1;
+                printf("%d ",i);
+                break;
+            }
+        }
+    }
+    if(c==0)
+    {
+        printf("-1");
+    }
+    
+}
