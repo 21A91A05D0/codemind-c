d3854b46a93cee0ea5b2adb6831fd55b3a33a280
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-06-09 15:24:07 +0530

    #include<stdio.h> int main() {     int a[100][100],arr[100];     int n,i,j,r,c,s=0,k=0,s1=0;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             if(i==0 || j==0 || i==r-1 || j==c-1)             {                 s+=a[i][j];             }         }     }     printf("%d",s); }

---
+++ Sum_of_all_boundary_values_in_a_matrix.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a[100][100],arr[100];
+    int n,i,j,r,c,s=0,k=0,s1=0;
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            if(i==0 || j==0 || i==r-1 || j==c-1)
+            {
+                s+=a[i][j];
+            }
+        }
+    }
+    printf("%d",s);
+}
