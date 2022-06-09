bc3d820a937c0882e23556980124171765689968
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-06-09 14:54:30 +0530

    #include<stdio.h> int main() {     int a[100][100],arr[100];     int n,i,j,r,c,s=0,k=0,l=0;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             s+=a[i][j];         }         arr[l++]=(s-k);         k=s;     }     int max=arr[0];     for(i=0;i<l;i++)     {         if(arr[i]>max)         {             max=arr[i];         }     }     printf("%d",max); }

---
+++ Maximum_sum_of_row_wise_sum_of_a_matrix.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int a[100][100],arr[100];
+    int n,i,j,r,c,s=0,k=0,l=0;
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            s+=a[i][j];
+        }
+        arr[l++]=(s-k);
+        k=s;
+    }
+    int max=arr[0];
+    for(i=0;i<l;i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+    printf("%d",max);
+}
