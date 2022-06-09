1e9be7ccfc27c862487563ec1a95dfbda7ac22d8
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-06-09 15:01:58 +0530

    #include<stdio.h> int main() {     int a[100][100],arr[100];     int n,i,j,r,c,s=0,k=0,l=0;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             s+=a[i][j];         }     }     printf("%d",s);
    }

---
+++ Sum_of_all_elements_in_a_matrix.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a[100][100],arr[100];
+    int n,i,j,r,c,s=0,k=0,l=0;
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            s+=a[i][j];
+        }
+    }
+    printf("%d",s);
+
+}
