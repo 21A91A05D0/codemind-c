5a9445457efa0b92dd966b2d361e6e7958253d1e
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-07-10 09:59:08 +0530

    #include<stdio.h> int main() {     int i,n,k=0,d,c=0,arr[100],f=0,t=0,a[100];     int sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=1;         for(int j=0;j<n;j++)         {             if(i!=j)             {                 if(arr[i]==arr[j])                 {                     c++;                     arr[j]=-2;                 }             }         }         if(c>=1)         {             a[k]=arr[i];             k++;         }     }     for(i=0;i<k;i++)     {         if(a[i]%2!=0)         {             t++;         }     }     printf("%d",t);
    }

--- odd_unique_elements_count.c
+++ odd_unique_elements_count.c
@@ -2,6 +2,7 @@
 int main()
 {
     int i,n,k=0,d,c=0,arr[100],f=0,t=0,a[100];
+    int sum=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
@@ -17,23 +18,23 @@ int main()
                 if(arr[i]==arr[j])
                 {
                     c++;
+                    arr[j]=-2;
                 }
             }
         }
-        if(c==1)
+        if(c>=1)
         {
-            f++;
             a[k]=arr[i];
             k++;
         }
-        
     }
-    for(k=0;k<f;k++)
+    for(i=0;i<k;i++)
     {
-        if(a[k]%2==1)
+        if(a[i]%2!=0)
         {
             t++;
         }
     }
     printf("%d",t);
+    
 }
